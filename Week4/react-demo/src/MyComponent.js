3fb325b6568133de744fede9da050676dbaac4d5
kailer <homework2265@gmail.com>
2024-10-24 16:59:33 -0500

    "finished hw 3"

---
+++ Week4/react-demo/src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+    return (
+        <div>
+            <h1> hello </h1>
+            <h1> world </h1>
+        </div>
+    )
+}
+
+export default MyComponent
